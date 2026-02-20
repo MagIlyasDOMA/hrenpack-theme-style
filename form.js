c80e572954dfe8c73ac1ffcea21d1574ea8b90a9
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2026-02-20 15:31:22 +0300

    3.7.2

--- form.js
+++ form.js
@@ -1,6 +1,6 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector(document.currentScript.dataset.formSelector || '.form');
+    const form = document.querySelector(document.currentScript?.dataset.formSelector || '.form');
     if (form)
         input_form_control_unline(form);
 });
--- form.ts
+++ form.ts
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form: HTMLFormElement | null = document.querySelector(
-        document.currentScript!.dataset.formSelector || '.form')
+        document.currentScript?.dataset.formSelector || '.form')
     if (form)
         input_form_control_unline(form)
 })
