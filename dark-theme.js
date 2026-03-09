6d3467b577457c806d9a97f01d6d9798ca91523a
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2026-03-09 18:32:56 +0300

    3.8.4

--- dark-theme.js
+++ dark-theme.js
@@ -10,8 +10,8 @@ const cookieTheme = getCookie?.('theme'), lsTheme = localStorage.getItem('theme'
 const theme_url_prefix = get_hts_url();
 const theme_light = theme_url_prefix + 'style_light.css', theme_dark = theme_url_prefix + 'style_dark.css';
 let currentTheme;
-const lightIcon = 'https://cdn.jsdelivr.net/npm/hrenpack-theme-style@3.8.0/toggle-theme/light.svg';
-const darkIcon = 'https://cdn.jsdelivr.net/npm/hrenpack-theme-style@3.8.0/toggle-theme/dark.svg';
+const lightIcon = 'https://raw.githubusercontent.com/MagIlyasDOMA/hrenpack-theme-style/refs/heads/data/toggle-theme/light.svg';
+const darkIcon = 'https://raw.githubusercontent.com/MagIlyasDOMA/hrenpack-theme-style/refs/heads/data/toggle-theme/dark.svg';
 if (cookieTheme)
     currentTheme = cookieTheme;
 else if (lsTheme)
